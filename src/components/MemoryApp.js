f1ea0c0757f2ae6ec422129212fef3ac91ca90ec
Cyril Khan <cyrilkhan8@gmail.com>
2018-04-09 12:56:05 +0200

    added AddContent component and handleAddContent event handler

--- src/components/MemoryApp.js
+++ src/components/MemoryApp.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import Card from './Card';
+import AddContent from './AddContent';
 
 export default class MemoryApp extends React.Component {
+  state = {
+    content: []
+  };
+  handleAddContent = (content) => {
+    // if there is an empty string
+    if (!content) {
+      return 'Valid value please';
+    // if there is already the same entry. indexOf returns index of option in array
+    // - 1 indicates a new entry. all other entries already have an index
+  } else if (this.state.content.indexOf(content) > -1) {
+      return 'This option already exists.';
+    }
+    // use concat return new array with merged content of original arrays
+    this.setState((prevState) => ({ content: prevState.content.concat(content) }));
+  };
   render() {
     return (
       <div>
         <h2>This renders correctly</h2>
+        <AddContent
+          options={this.state.content}
+          handleAddContent={this.handleAddContent}
+          />
         <Card />
       </div>
     );
