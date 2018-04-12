d63efa1886eb4d761feb39f150a77bc336a3288a
Cyril Khan <cyrilkhan8@gmail.com>
2018-04-12 09:03:06 +0200

    added deleteButton with fade animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,35 +2,41 @@ import React from 'react';
 
 export default class Card extends React.Component {
   state = {
-    active: false
+    active: false,
+    deleteButtonActive: false
   };
   handleFlip = () => {
     const currentState = this.state.active
     this.setState(() => ({ active: !currentState }));
     this.props.handleSameContent(this.props.backContent);
   };
+  handleDisappear = (e) => {
+    e.stopPropagation();
+    const deleteBtnState = this.state.deleteButtonActive;
+    this.setState(() => ({ deleteButtonActive: !deleteBtnState }));
+    console.log('handleDisappear fired');
+  };
   render() {
     return (
       <div>
+        <div className={this.state.deleteButtonActive ? 'container-hide' : null}>
           <section className="container">
-          <div
-            id="card"
-            className={this.state.active ? 'flipped' : null}
-            onClick={this.handleFlip}>
-            <figure className="front"><span>{this.props.count}</span></figure>
-            <figure className="back">
-              <span>{this.props.backContent}</span>
-              <button
-                className="card__button"
-                onClick={(e) => {
-                  this.props.handleDeleteContent(this.props.backContent);
-                  e.stopPropagation();
-                }}
-                >x
-              </button>
-            </figure>
-          </div>
-        </section>
+            <div
+              id="card"
+              className={this.state.active ? 'flipped' : null}
+              onClick={this.handleFlip}>
+              <figure className="front"><span>{this.props.count}</span></figure>
+              <figure className="back">
+                <span>{this.props.backContent}</span>
+                <button
+                  className="card__button"
+                  onClick={this.handleDisappear}
+                  >x
+                </button>
+              </figure>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
